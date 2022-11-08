74a9e16cf169f94bd36309080f9e73dd30f93f93
pa4080 <spas.z.spasov@gmail.com>
2022-11-08 22:48:46 +0200

    Testimonials slider rebuild: Stage 2

--- views-elements/Testimonials.php
+++ views-elements/Testimonials.php
@@ -1,25 +1,81 @@
 <div class="testimonials-wrapper">
     <div class="testimonials">
-        <div class="testimonials__thumb__wrapper">
-            <div class="testimonials__slider">
+        <?php
+        $dir_thumb = "/public/testimonials/webp-thumb";
+        $dir_big =  "/public/testimonials/webp";
+        $dir_video = "/public/testimonials/webm";
+        $images = scandir(APP_ROOT . $dir_thumb);
 
-                <div class="testimonials__thumb__single">
-                    <img src="/public/images/videos_old-testimonials-to-delete/video_2_thumb.webp" width="200" height="113" alt="Media File video_2_thumb.webp" loading="lazy" decoding="async" />
-                </div>
+        // Tidy up the array ---
+        // https://stackoverflow.com/questions/7225070/php-array-delete-by-value-not-key
+        $images = array_diff($images, array('.', '..'));
+        shuffle($images);
 
-                <div class="testimonials__thumb__single">
-                    <img src="/public/images/videos_old-testimonials-to-delete/video_3_thumb.webp" width="200" height="113" alt="Media File video_3_thumb.webp" loading="lazy" decoding="async" />
-                </div>
+        // If some of the items related to the gallery,
+        // doesn't exist remove it from the $images array
+        foreach ($images as $image) {
+            $thumb = "$dir_thumb/$image";
+            $big = "$dir_big/$image";
+            $video = "$dir_video/" . str_replace(".webp", ".webm", $image);
 
-                <div class="testimonials__thumb__single">
-                    <img src="/public/images/videos_old-testimonials-to-delete/video_4_thumb.webp" width="200" height="113" alt="Media File video_4_thumb.webp" loading="lazy" decoding="async" />
-                </div>
+            if (!is_file(APP_ROOT . $thumb) || !is_file(APP_ROOT . $big) || !is_file(APP_ROOT . $video)) {
+                $images = array_diff($images, array($image));
+            }
+        }
+        shuffle($images);
 
-                <div class="testimonials__thumb__single">
-                    <img src="/public/images/videos_old-testimonials-to-delete/video_5_thumb.webp" width="200" height="113" alt="Media File video_5_thumb.webp" loading="lazy" decoding="async" />
-                </div>
+        // Output the gallery
+        foreach ($images as $index => $image) {
+            $thumb = "$dir_thumb/$image";
+            $big = "$dir_big/$image";
+            $video = "$dir_video/" . str_replace(".webp", ".webm", $image);
 
-            </div>
-        </div>
+            // Set the default classes for the thumb images
+            $thumb_container_classes = "testimonials__thumb__single" . " ";
+
+            if ($index === 0) {
+                // Output the first big image
+                printf(
+                    '<div class="%s">
+                        <a class="testimonials-image-item" href="%s" area-label="%s">
+                            <img class="%s" width="%u" height="%u" loading="lazy" decoding="async" src="%s" alt="%s" />
+                        </a>
+                    </div>
+                    <div class="testimonials__thumb__wrapper"><div class="testimonials__slider">', // Open the thumb slider
+                    "testimonials__selected",
+                    $video,
+                    "Testimonials video",
+                    "testimonials-image-effect",
+                    996,
+                    560,
+                    $big,
+                    "Testimonials HQ image"
+                );
+
+                // Add the active class to the first thumb image
+                $thumb_container_classes .= "testimonials__thumb__selected";
+            }
+
+            printf(
+                '<div class="%s">
+                    <a class="testimonials-image-item" href="%s" area-label="%s">
+                        <img class="%s" width="%u" height="%u" loading="lazy" decoding="async" src="%s" alt="%s" />
+                    </a>
+                </div>',
+                $thumb_container_classes,
+                $big,
+                "Testimonials HQ image",
+                "",
+                200,
+                113,
+                $thumb,
+                "Testimonials thumb image"
+            );
+
+            if ($index === count($images) - 1) {
+                printf('</div></div>'); // Close the thumb slider
+            }
+        }
+        ?>
     </div>
 </div>
